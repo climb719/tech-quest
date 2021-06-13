90f24b3a82bf9962e96a5f9835682d8117f0ad95
climb719 <mary.oldweiler@gmail.com>
2021-06-13 09:15:42 -0400

    add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   resources :comments
   resources :positions
   resources :interview_questions
   resources :users
+  #routes order matters so custom routes don't get associated with an id
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
