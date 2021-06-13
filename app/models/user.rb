092ba4455017a8d4d9b99fe4e56513acd8bc3053
climb719 <mary.oldweiler@gmail.com>
2021-06-13 09:10:29 -0400

    add has_secure_password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :interview_questions
     has_many :comments
     has_many :commented_questions, through: :comments, source: :interview_question
+    has_secure_password #gives .authenticate and validations for password, needs bycrpt gem
 end
