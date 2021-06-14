4e8d02cee8c0b0fc0c5760d35aee8d66f39b5054
climb719 <mary.oldweiler@gmail.com>
2021-06-14 10:51:50 -0400

    new form for IQ

--- app/controllers/interview_questions_controller.rb
+++ app/controllers/interview_questions_controller.rb
@@ -1,2 +1,13 @@
 class InterviewQuestionsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def index
+        @interview_questions = InterviewQuestion.all
+    end
+
+    def new
+        @interview_question = InterviewQuestion.new
+    end
+
+    
 end
