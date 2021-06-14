d4e199820f46da1d1fcde39b5bc65732830d452a
climb719 <mary.oldweiler@gmail.com>
2021-06-14 09:52:06 -0400

    login -create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,17 @@ class SessionsController < ApplicationController
         session.clear
         redirect_to root_path
     end
+
+    def create
+        #find user - not creating or updatinf user, don't need instance variable 
+        user = User.find_by(email: params[:user][:email])
+        #check if find the user AND passowrd valid
+        if user && user.authenticate(params[:user][:pass])
+        #set the session
+        session[:user_id] = user.id
+        redirect_to user_path(user) #/users/#{user.if}
+        #else redirect to login vs. rendering so nothing prepopulats and form clears completely and does give any info about what part was wrong 
+        flash[:massage] = "Oops, looks like your login info is incorrect. Please try again."
+        redirect_to "/login"
+    end
 end
