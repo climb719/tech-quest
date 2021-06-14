33d1c81d2061f6f76098b9f74506223a9f9bb2c0
climb719 <mary.oldweiler@gmail.com>
2021-06-14 11:47:56 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Position.create(title: "Software Engineer")
+Position.create(title: "Software Developer")
+Position.create(title: "Front End Developer")
+Position.create(title: "Network Engineer")
+Position.create(title: "Java Developer")
+Position.create(title: "Junior Web Developer")
+Position.create(title: "Full Stack Developer")
+Position.create(title: "Entry Level Web Developer")
+Position.create(title: "Entry Level Software Engineer")
+Position.create(title: "Junior Developer")
+Position.create(title: "SQL Developer")
+Position.create(title: "Programmer") 
+	
+
+
+
+    
