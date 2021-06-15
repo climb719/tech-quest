3b11591f194232fab12e5d4bdc100da29e445963
climb719 <mary.oldweiler@gmail.com>
2021-06-15 11:01:24 -0400

    create iq method

--- app/controllers/interview_questions_controller.rb
+++ app/controllers/interview_questions_controller.rb
@@ -9,5 +9,22 @@ class InterviewQuestionsController < ApplicationController
         @interview_question = InterviewQuestion.new
     end
 
+    def create
+        #binding.pry
+        @interview_question = current_user.interview_questions.build(interview_question_params)
+        #binding.pry
+        if @interview_question.save
+            redirect_to interview_questions_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def interview_question_params
+        params.require(:interview_question).permit(:question, :answer, :category_id)
+    end
+
     
 end
