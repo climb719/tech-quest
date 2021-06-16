36ff0c295806efb81dd9509131984282c5cf0c74
climb719 <mary.oldweiler@gmail.com>
2021-06-16 15:02:38 -0400

    add category index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,17 @@
 class CategoriesController < ApplicationController
+
+    def index
+        @categories = Category.all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+
 end
