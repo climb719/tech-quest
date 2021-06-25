ff1652d5226c7896a3d7676a090444866fbff8b6
climb719 <mary.oldweiler@gmail.com>
2021-06-25 02:19:33 -0400

    log in with google button

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,9 @@
 #middleware interpret request and transform so accurate request for outside party
 #instructions how to access it with provider (from google documentation to access)
 #send in id and secret keys through omniauth
-#access like a class ENV similar to hash, part of left is a key 
+#access like a class ENV similar to hash, part of left is a key
+#skip javascript web token to help request go through
+# setting up sp when we make link to visit, we can interpret request and transform it to request to 3rd party site
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], :skip_jwt => true
 end 
