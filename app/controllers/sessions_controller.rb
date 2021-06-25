d59e144f5c07e3e08042690740aba59fd0f914f0
climb719 <mary.oldweiler@gmail.com>
2021-06-25 05:21:44 -0400

    omniauth testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,39 +24,40 @@ class SessionsController < ApplicationController
     end
 
     def omniauth
-        #sole responsibility if logging in with oauth
-        #method for logging in only with omniauth
-        # if usere has logged in this way, find them
-        #if not, create 
+       # byebug
+        # #sole responsibility if logging in with oauth
+        # #method for logging in only with omniauth
+        # # if usere has logged in this way, find them
+        # #if not, create 
         @user = User.find_or_create_by(email: auth["info"]["email"]) do |u|
             u.name = auth["info"]["first_name"]
             u.password = SecureRandom.hex(17)
-            #password needs to be assigned because macro has_secure_password validates for presence of passowrd
-            # validates that met requierments to be an object 
+        #     #password needs to be assigned because macro has_secure_password validates for presence of passowrd
+        #     # validates that met requierments to be an object 
 
-        #find or create by to find the user with that email or create them, but want to try to find by parameters
-        #create with additional parameters
-        #pass in block tht are only used by create by
-        #find by whats passed in or create by whats passed in PLUS what is in do bloack
-        #before going to 3rd party need to escape app
-        end
+        # #find or create by to find the user with that email or create them, but want to try to find by parameters
+        # #create with additional parameters
+        # #pass in block tht are only used by create by
+        # #find by whats passed in or create by whats passed in PLUS what is in do bloack
+        # #before going to 3rd party need to escape app
+         end
 
     #check if the registered correctly
         if @user.save
-                session[:user_id] = @.id
-                redirect_to user_path(user)
+                session[:user_id] = @user.id
+                redirect_to user_path(@user)
         else
             flash[:message] = "Something went wrong"
             redirect_to '/'
-    #log them in 
+    # #log them in 
         end
     end
 
     # private 
 
-    # def auth #private method not to have to rewrite many times, vccan use like a has 
-    #     request.env['omniauth.auth']
-    # end
+     def auth #private method not to have to rewrite many times, vccan use like a has 
+        request.env['omniauth.auth']
+     end
 
 
 end
