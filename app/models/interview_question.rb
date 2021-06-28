33e4b660c06199131a994980b87e429b4ec0c762
climb719 <mary.oldweiler@gmail.com>
2021-06-28 09:42:58 -0400

    render partials

--- app/models/interview_question.rb
+++ app/models/interview_question.rb
@@ -5,6 +5,9 @@ class InterviewQuestion < ApplicationRecord
   has_many :users, through: :comments
   #accepts_nested_attributes_for :category
   validates :question, :answer, :category, presence: true 
+ # field with errors shows up - form for accepts an object, if excepts an object with an error, it will add div -field with errors
+ # but page must be rendered, if redirect, will loose data - also check if content doesn't dissapear
+  # add other validations? 
 end
 
 
