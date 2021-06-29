ab6620b1da40ea7e325b3f494105644ae67beeed
climb719 <mary.oldweiler@gmail.com>
2021-06-29 15:24:42 -0400

    update iq helpers

--- app/helpers/interview_questions_helper.rb
+++ app/helpers/interview_questions_helper.rb
@@ -1,5 +1,13 @@
 module InterviewQuestionsHelper
 
+    def index_view_header
+        if @user
+            "#{@user.name}'s Interview Questions"
+        else
+            "All Interview Questions"
+        end
+    end
+
     def new_question_header
         if @interview_question.category
           "New Interview Question for #{@interview_question.category.name}"
@@ -16,4 +24,11 @@ end
 # <% else %>
 # <h1>Add a new Interview Question here:</h1>
 # <% end %>
+
+# <h1> <%=current_user.name%> </h1>
+
+# <% if @users_questions.empty? %>
+ 
+# <h2>Looks like you haven't added any interview questions yet. 
+# Post a question <%= link_to "here", new_interview_questions_path%> </h2>
 
