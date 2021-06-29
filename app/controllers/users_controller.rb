e2af6a0b9f6e6942af64672f561a1f42ad17be65
climb719 <mary.oldweiler@gmail.com>
2021-06-29 13:07:49 -0400

    oops login link to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
     def index
         @users_questions = current_user.categories.by_category
         #binding.pry
-    
     end
 
+    def new
+        @user = User.new
+    end
 
     def create
             @user = User.new(user_params)
