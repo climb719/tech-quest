6509043943cdcca0f211373e97b334546019afe9
climb719 <mary.oldweiler@gmail.com>
2021-06-29 08:53:56 -0400

    helpers iqs

--- app/helpers/interview_questions_helper.rb
+++ app/helpers/interview_questions_helper.rb
@@ -1,10 +1,17 @@
 module InterviewQuestionsHelper
 
     def new_question_header
-        <% if @interview_question.category %>
-        <h1>New Interview Question for:</h1> <br> <h2><%= @interview_question.category.name %></h2> 
-        <% else %>
-        <h1>Add a new Interview Question here:</h1>
-        <% end %>
+        if @interview_question.category
+          "New Interview Question for #{@interview_question.category.name}"
+        else
+         "Add a new Interview Question here:"
+        end 
     end
 end
+
+
+# <% if @interview_question.category %>
+# <h1>New Interview Question for:</h1> <br> <h2><%= @interview_question.category.name %></h2> 
+# <% else %>
+# <h1>Add a new Interview Question here:</h1>
+# <% end %>
