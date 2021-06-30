0c162c469f985235edf8cbe41779ec95df765e51
climb719 <mary.oldweiler@gmail.com>
2021-06-30 14:04:53 -0400

    add category validations

--- app/models/interview_question.rb
+++ app/models/interview_question.rb
@@ -8,13 +8,15 @@ class InterviewQuestion < ApplicationRecord
  # field with errors shows up - form for accepts an object, if excepts an object with an error, it will add div -field with errors
  # but page must be rendered, if redirect, will loose data - also check if content doesn't dissapear
   #belongs to category so have getter and setter for thus has a category for an attribute 
-  accepts_nested_attributes_for :category
+  accepts_nested_attributes_for :category,  reject_if: proc { |attributes| attributes['name'].blank? }
+   #setter method macro
   #uses category_attributes method that accepts a hash of attributes and when it accepts hash it creates a new category
   #this accepts it from model and params come through right 
+  # above writing the method:
   # def category_attributes=(attribute)
-  #   self.category = Category.find_or_create_by(attribute) if !attribute[:name].blank?
+  #   self.category = Category.find_or_create_by(attribute) 
   # end
-
+#reject if there because if blank will override category id selected, so need to tell it to reject if blank
  
 # add other validations? 
 end
