10d9411d6e42bbde5bc74f50986d932f99f257c2
climb719 <mary.oldweiler@gmail.com>
2021-06-30 15:42:55 -0400

    display date method for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :interview_question
 
   validates :content, presence: true 
+
+
+  def display_date
+    self.created_at.strftime("%d %b. %Y")
+  end
 end
