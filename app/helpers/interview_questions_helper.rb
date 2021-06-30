9663b81525000a2fcf4664d878022f7581f7caf5
climb719 <mary.oldweiler@gmail.com>
2021-06-30 16:00:52 -0400

    test helper methods for iq

--- app/helpers/interview_questions_helper.rb
+++ app/helpers/interview_questions_helper.rb
@@ -16,19 +16,38 @@ module InterviewQuestionsHelper
         end 
     end
 
+    # def display_categories_and_questions
+    #     @interview_questions.each do |category| 
+    #         content_tag(:h3, "{category.name}")  if !category.interview_questions.empty? 
+    #         category.interview_questions.each do |iq| 
+    #            if @user 
+    #            tag.h2(link_to "#{iq.question}", interview_question_path(iq)) if iq.user == current_user  
+        
+    #             else 
+    #                 tag.h2(link_to "#{iq.question}", interview_question_path(iq))   
+          
+    #             end  
+    #         end    
+        
+    #     end 
+    # end
+
 end
 
 
-# <% if @interview_question.category %>
-# <h1>New Interview Question for:</h1> <br> <h2><%= @interview_question.category.name %></h2> 
-# <% else %>
+#render partial: "questions", locals: {interview_questions: @interview_questions} %>
+
+
+# if @interview_question.category 
+# <h1>New Interview Question for:</h1> <br> <h2><%= @interview_question.category.name </h2> 
+# else 
 # <h1>Add a new Interview Question here:</h1>
-# <% end %>
+# end 
 
-# <h1> <%=current_user.name%> </h1>
+# <h1> <%=current_user.name </h1>
 
-# <% if @users_questions.empty? %>
+# if @users_questions.empty? 
  
 # <h2>Looks like you haven't added any interview questions yet. 
-# Post a question <%= link_to "here", new_interview_questions_path%> </h2>
+# Post a question <%= link_to "here", new_interview_questions_path </h2>
 
