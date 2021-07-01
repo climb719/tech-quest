a17c4e0a6679e46c45ff8602c79fdb6401bae711
climb719 <mary.oldweiler@gmail.com>
2021-07-01 14:58:55 -0400

    refactoring user helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,14 @@
 module UsersHelper
 
+    
+    def display_users_questions
+        if @user.interview_questions.empty? 
+            content_tag(:h2, "Looks like you haven't added any interview questions yet. Post a question <a href='/interview_questions/new'>HERE</a>")
+        else
+            content_tag(:h3, "My Interview Questions")
+             render partial: "interview_questions/questions", locals: {interview_questions: @users_questions} 
+        end
+    end
+
 
 end
