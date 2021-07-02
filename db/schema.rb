d993e54001db4694eae8f79eb965766c891560f2
climb719 <mary.oldweiler@gmail.com>
2021-07-02 10:22:04 -0400

    remove description from categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_195525) do
+ActiveRecord::Schema.define(version: 2021_07_02_141923) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
