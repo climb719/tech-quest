724ab6855a2863f23be0ca3a14490067cdf1719c
climb719 <mary.oldweiler@gmail.com>
2021-07-02 09:56:57 -0400

    categories helper

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,4 +12,11 @@ class Comment < ApplicationRecord
   def self.order_and_limit
     Comment.all.order('created_at DESC').take(3)
   end
+  #class method or make scope ?
+
 end
+
+
+
+# <% @interview_question.comments.sort_by{|t| - t.created_at.to_i}.first(3).each do |c| %>
+#   <li> <%= c.user.name %> said "<%= link_to truncate(c.content, :length => 65), comment_path(c) %>" on <%= c.created_at.strftime("%d %b. %Y") %>  </li>
