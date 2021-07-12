57eb12a7411cecd8995f81f5b456e89f60451d91
climb719 <mary.oldweiler@gmail.com>
2021-07-12 09:44:59 -0400

    add except to comments route

--- config/routes.rb
+++ config/routes.rb
@@ -5,31 +5,24 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
-
-  #login - 2 routes - display login and process
+ 
   get '/login' => 'sessions#new'
   post '/login'  => 'sessions#create'
 
-  #logout route - delete since deleting the session
   delete '/logout' => 'sessions#destroy'
 
-  
-  resources :comments
+  resources :comments, except: [:new, :create]
 
   resources :categories do
     resources :interview_questions, only: [:new, :create, :index] 
   end
+
   resources :interview_questions do
     resources :comments, only: [:new, :create]
   end
+  
   resources :users do
     resources :interview_questions, only: [:index]
   end
-  #  resources :users, only: [:new, :create, :show] 
-  
-  
-  #get '/:my_questions' => 'users#index'
-  
-  #routes order matters so custom routes don't get associated with an id
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
 end
